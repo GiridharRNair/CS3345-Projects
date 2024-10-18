4afe6ddfaefb6bbb8fc2d543af1b760733eec589
Giridhar Nair <Giridhar.r.nair@gmail.com>
2024-10-18 12:17:55 -0500

    finished BST

---
+++ project_2/BinarySearchTree.java
@@ -0,0 +1,277 @@
+/**
+ * @author rbk, sa
+ * Binary search tree (starter code)
+ **/
+
+// replace package name with your netid
+package gxn210004;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BinarySearchTree<T extends Comparable<? super T>>
+    implements Iterable<T> {
+
+    static class Entry<T> {
+
+        T element;
+        Entry<T> left, right;
+
+        public Entry(T x, Entry<T> left, Entry<T> right) {
+            this.element = x;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    Entry<T> root;
+    int size;
+
+    // define stack
+    // Stack<Entry<T>> stack = new Stack<>();
+
+    static class BSTIterator<T> implements Iterator<T> {
+
+        Stack<Entry<T>> stack = new Stack<>();
+
+        public BSTIterator(Entry<T> root) {
+            pushLeft(root);
+        }
+
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        public T next() {
+            Entry<T> node = stack.pop();
+            pushLeft(node.right);
+            return node.element;
+        }
+
+        public void pushLeft(Entry<T> node) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+    }
+
+    public BinarySearchTree() {
+        root = null;
+        size = 0;
+    }
+
+    /** TO DO: Is x contained in tree?
+     */
+    public boolean contains(T x) {
+        Entry<T> node = root;
+        while (node != null) {
+            int cmp = x.compareTo(node.element);
+            if (cmp == 0) {
+                return true;
+            } else if (cmp < 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return false;
+    }
+
+    /** TO DO: Add x to tree.
+     *  If tree contains a node with same key, replace element by x.
+     *  Returns true if x is a new element added to tree.
+     */
+    public boolean add(T x) {
+        if (root == null) {
+            root = new Entry<>(x, null, null);
+            size++;
+            return true;
+        }
+        Entry<T> node = root;
+        Entry<T> parent = null;
+        int cmp = 0;
+        while (node != null) {
+            cmp = x.compareTo(node.element);
+            if (cmp == 0) {
+                node.element = x;
+                return false;
+            } else if (cmp < 0) {
+                parent = node;
+                node = node.left;
+            } else {
+                parent = node;
+                node = node.right;
+            }
+        }
+        Entry<T> newNode = new Entry<>(x, null, null);
+        if (cmp < 0) {
+            parent.left = newNode;
+        } else {
+            parent.right = newNode;
+        }
+        size++;
+        return true;
+    }
+
+    /** TO DO: Remove x from tree.
+     *  Return x if found, otherwise return null
+     */
+    public T remove(T x) {
+        Entry<T> node = root;
+        Entry<T> parent = null;
+        while (node != null) {
+            int cmp = x.compareTo(node.element);
+            if (cmp == 0) {
+                T result = node.element;
+                if (node.left == null || node.right == null) {
+                    bypass(node, parent);
+                } else {
+                    Entry<T> minRight = node.right;
+                    Entry<T> minRightParent = node;
+                    while (minRight.left != null) {
+                        minRightParent = minRight;
+                        minRight = minRight.left;
+                    }
+                    node.element = minRight.element;
+                    bypass(minRight, minRightParent);
+                }
+                size--;
+                return result;
+            } else if (cmp < 0) {
+                parent = node;
+                node = node.left;
+            } else {
+                parent = node;
+                node = node.right;
+            }
+        }
+        return null;
+    }
+
+    public void bypass(Entry<T> node, Entry<T> parent) {
+        Entry<T> child = (node.left != null) ? node.left : node.right;
+        if (parent == null) {
+            root = child;
+        } else if (node == parent.left) {
+            parent.left = child;
+        } else {
+            parent.right = child;
+        }
+    }
+
+    // Start of Optional problems
+
+    /** Optional problem : Iterate elements in sorted order of keys
+     Solve this problem without creating an array using in-order traversal (toArray()).
+     */
+    public Iterator<T> iterator() {
+        return new BSTIterator<>(root);
+    }
+
+    // Optional problem
+    public T min() {
+        return null;
+    }
+
+    public T max() {
+        return null;
+    }
+
+    // Optional problem.  Find largest key that is no bigger than x.  Return null if there is no such key.
+    public T floor(T x) {
+        return null;
+    }
+
+    // Optional problem.  Find smallest key that is no smaller than x.  Return null if there is no such key.
+    public T ceiling(T x) {
+        return null;
+    }
+
+    // Optional problem.  Find predecessor of x.  If x is not in the tree, return floor(x).  Return null if there is no such key.
+    public T predecessor(T x) {
+        return null;
+    }
+
+    // Optional problem.  Find successor of x.  If x is not in the tree, return ceiling(x).  Return null if there is no such key.
+    public T successor(T x) {
+        return null;
+    }
+
+    // Optional: Create an array with the elements using in-order traversal of tree
+    public Comparable[] toArray() {
+        Comparable[] arr = new Comparable[size];
+        /* write code to place elements in array here */
+        return arr;
+    }
+
+    // End of Optional problems
+
+    public static void main(String[] args) throws FileNotFoundException {
+        BinarySearchTree<Long> bst = new BinarySearchTree<>();
+        Scanner sc;
+        if (args.length > 0) {
+            File file = new File(args[0]);
+            sc = new Scanner(file);
+        } else {
+            sc = new Scanner(System.in);
+        }
+        String operation = "";
+        long operand = 0;
+        int modValue = 999983;
+        long result = 0;
+        // Initialize the timer
+        // Timer timer = new Timer();
+
+        while (!((operation = sc.next()).equals("End"))) {
+            switch (operation) {
+                case "Add": {
+                    operand = sc.nextInt();
+                    if (bst.add(operand)) {
+                        result = (result + 1) % modValue;
+                    }
+                    break;
+                }
+                case "Remove": {
+                    operand = sc.nextInt();
+                    if (bst.remove(operand) != null) {
+                        result = (result + 1) % modValue;
+                    }
+                    break;
+                }
+                case "Contains": {
+                    operand = sc.nextInt();
+                    if (bst.contains(operand)) {
+                        result = (result + 1) % modValue;
+                    }
+                    break;
+                }
+            }
+        }
+
+        // End Time
+        // timer.end();
+
+        System.out.println(result);
+        // System.out.println(timer);
+    }
+
+    public void printTree() {
+        System.out.print("[" + size + "]");
+        printTree(root);
+        System.out.println();
+    }
+
+    // Inorder traversal of tree
+    void printTree(Entry<T> node) {
+        if (node != null) {
+            printTree(node.left);
+            System.out.print(" " + node.element);
+            printTree(node.right);
+        }
+    }
+}
